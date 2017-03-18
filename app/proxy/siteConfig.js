56f7556236a489b82b60aa3bb879870ec271e6f5
xwatson <wusheng.xu@oborcloud.com>
2017-03-18 18:50:51 +0800

    添加站点配置代理

---
+++ app/proxy/siteConfig.js
@@ -0,0 +1,20 @@
+/**
+ * Created by xuwus on 2017/3/13.
+ */
+const SiteConfig = require('../models/siteConfig')
+
+/**
+ * 获取站点配置信息
+ * @returns {Promise.<void>}
+ */
+exports.getConfig = async() => {
+    return await SiteConfig.findOne()
+}
+
+exports.createConfig = async(config) => {
+    let user = await SiteConfig.findOne()
+    if (user) {
+        return user
+    }
+    return await SiteConfig.create(config)
+}
