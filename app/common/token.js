5cf0f8a8b277fb1201c5d591cba7a0d0102b701b
Xwatson <xuwusheng@hotmail.com>
2017-03-23 23:38:50 +0800

    添加token验证

--- app/common/token.js
+++ app/common/token.js
@@ -2,11 +2,55 @@
  * Created by xuwus on 2017/3/22.
  */
 const jwt = require('jwt-simple')
-var jwtTokenSecret = Symbol('NEWS_RELEASE_LOVE_LXJ_TOKEN')
+const jwtTokenSecret = Symbol('NEWS_RELEASE_LOVE_LXJ_TOKEN')
+const responseCode = require('../common/responseCode')
 
+/**
+ * 生成token
+ * @param _iss id
+ * @param expires 时效
+ * @returns {String}
+ */
 module.exports.getToken = (_iss, expires) => {
     return jwt.encode({
         iss: _iss,
         exp: expires
     }, jwtTokenSecret)
+}
+
+/**
+ * 验证token
+ * @param req
+ * @param res
+ * @param next
+ */
+module.exports.verifyToken = function (req,res,next) {
+    const message = {}
+    const token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['news-access-token']
+    if (token) {
+        try {
+            var decoded = jwt.decode(token, jwtTokenSecret)
+            if (decoded.exp <= Date.now()) {
+                message.code = responseCode.AUTH_EXPIRED
+                message.message = '登录超时'
+                res.body = message
+                res.end()
+                return
+            }
+            req.uid=decoded.iss
+        } catch (err) {
+            message.code = responseCode.AUTH_EXPIRED
+            message.message = '登录超时'
+            res.body = message
+            res.end()
+            return
+        }
+        next()
+    } else {
+        message.code = responseCode.AUTH_EXPIRED
+        message.message = '登录超时'
+        res.body = message
+        res.end()
+        return
+    }
 }
