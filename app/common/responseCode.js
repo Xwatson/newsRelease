3f1b3f54b90a8b88fb26cdf5601d29993636ec15
Xwatson <xuwusheng@hotmail.com>
2017-03-23 23:34:58 +0800

    添加返回状态码

---
+++ app/common/responseCode.js
@@ -0,0 +1,12 @@
+/**
+ * Created by xuwus on 2017/3/23.
+ */
+
+module.exports = {
+    SUCCESS              : 1,
+    FAIL                 : 2,
+    SIGN_FAIL            : 1000,
+    API_VERSION_WRONG    : 1001,
+    AUTH_EXPIRED         : 1002,
+    SKU_STOCK_NOT_ENOUGH : 2000
+}
