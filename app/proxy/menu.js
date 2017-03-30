975cc8e0f459993735fae05c13b5de194615cf69
Xwatson <xuwusheng@hotmail.com>
2017-03-30 23:00:16 +0800

    添加菜单排序、置顶的proxy

--- app/proxy/menu.js
+++ app/proxy/menu.js
@@ -1,7 +1,7 @@
 /**
  * Created by xwatson on 2017/3/30.
  */
-const sequelize = require("../models/sequelize.js")
+const sequelize = require("../models/sequelize")
 const Menu = require('../models/menu')
 
 /**
@@ -19,15 +19,15 @@ exports.create = async(menu) => {
  */
 exports.update = async(menu) => {
     return await Menu.update({
-        name:menu.name,
-        router:menu.router,
-        icon:menu.icon,
-        // sort:menu.sort, //不更新排序
-        parent_id:menu.parent_id,
-        status:menu.status,
-    },{
-        where:{
-            id:menu.id
+        name: menu.name,
+        router: menu.router,
+        icon: menu.icon,
+        // sort:menu.sort, // 不更新排序
+        parent_id: menu.parent_id,
+        status: menu.status,
+    }, {
+        where: {
+            id: menu.id
         }
     })
 }
@@ -38,26 +38,31 @@ exports.update = async(menu) => {
  * @returns {Promise.<void>}
  */
 exports.sort = async(current, exchange) => {
-    /*sequelize.transaction(() => {
-        return Menu.update({ sort:current.sort },{ where:{ id:exchange.id } })
-            .then(() => {})
-    })*/
-
-    try {
-        const exchangeUpdate = await
-        if (exchangeUpdate) {
-            return await Menu.update({
-                sort:exchange.sort
-            },{
-                where:{
-                    id:current.id
-                }
+    // 启动事务
+    return await sequelize.transaction((t) => {
+        return Menu.update({sort: exchange.sort}, {where: {id: current.id}}, {transaction: t})
+            .then(() => {
+                return Menu.update({sort: exchange.sort}, {where: {id: current.id}}, {transaction: t})
             })
-        } else {
-            return null
-        }
+            .then(t.commit.bind(t))
+            .catch((err) => {
+                console.error(err)
+                throw err
+            })
+    })
+}
+/**
+ * 置頂
+ * @param menu
+ * @returns {Promise.<void>}
+ */
+exports.stickTop = async(menu) => {
+    try {
+        return await Menu.max('sort').then((sort) => {
+            return Menu.update({sort: sort + 1}, {where: {id: menu.id}})
+        })
     } catch (err) {
+        console.log(err)
         throw err
     }
-
 }
