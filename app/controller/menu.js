caa6b0e6083208882b7c514a28dbc602765b73e2
xwatson <wusheng.xu@oborcloud.com>
2017-04-15 20:35:30 +0800

    修改菜单ctrl

--- app/controller/menu.js
+++ app/controller/menu.js
@@ -11,28 +11,32 @@ const errLog = '菜单控制器：'
  * @returns {Promise.<void>}
  */
 exports.create = async(ctx) => {
-    const data = ctx.body
+    const data = ctx.request.body
     const message = {}
     try {
         let menu = await Menu.getMenuByName(data.name)
-        if (!menu) {
-            menu = await Menu.create({
-                name:data.name,
-                router:data.router,
-                icon:data.icon,
-                sort:Menu.max('sort', { where:{ parent_id:data.parent_id || 0 } }),
-                parent_id:data.parent_id || 0
-            })
-            if (menu) {
-                message.code = responseCode.SUCCESS
-                message.message = '创建成功'
-            } else {
-                message.code = responseCode.FAIL
-                message.message = '创建失败'
-            }
-        } else {
+        if (menu) {
             message.code = responseCode.FAIL
             message.message = '菜单名称已存在'
+            ctx.body = message
+            return ctx
+        }
+        menu = await Menu.create({
+            name:data.name,
+            router:data.router,
+            icon:data.icon,
+            sort:Menu.max('sort', { where:{ parent_id:data.parent_id || 0 } }),
+            parent_id:data.parent_id || 0,
+            status:data.status
+        })
+        if (menu) {
+            menu = await Menu.getMenuByName(data.name)
+            message.code = responseCode.SUCCESS
+            message.message = '创建成功'
+            message.data = menu
+        } else {
+            message.code = responseCode.FAIL
+            message.message = '创建失败'
         }
         ctx.body = message
         return ctx
@@ -47,7 +51,7 @@ exports.create = async(ctx) => {
  * @returns {Promise.<void>}
  */
 exports.update = async(ctx) => {
-    const data = ctx.body
+    const data = ctx.request.body
     const message = {}
     try {
         if (data.id) {
@@ -70,6 +74,8 @@ exports.update = async(ctx) => {
             message.code = responseCode.FAIL
             message.message = '未找到id字段'
         }
+        ctx.body = message
+        return ctx
     } catch (err) {
         console.log(`${errLog}修改菜单出错：`, err)
         throw err
@@ -85,15 +91,32 @@ exports.delete = async(ctx) => {
     const message = {}
     try {
         if (data.id) {
-            const menu = await Menu.delete(data.id)
-            if (menu) {
-                message.code = responseCode.SUCCESS
-                message.message = '删除成功'
-                message.data = menu
-            } else {
+            let menu = await Menu.getMenuById(data.id)
+            if (!menu) {
+                message.code = responseCode.FAIL
+                message.message = 'id不存在'
+                ctx.body = message
+                return ctx
+            }
+            const authMenu = await menu.getAuthMenu()
+            if (authMenu.length) {
+                message.code = responseCode.FAIL
+                message.message = '该操菜单已被关联'
+                ctx.body = message
+                return ctx
+            }
+            menu = await Menu.delete(data.id)
+            if (!menu) {
                 message.code = responseCode.FAIL
                 message.message = '删除失败'
+                ctx.body = message
+                return ctx
             }
+            message.code = responseCode.SUCCESS
+            message.message = '删除成功'
+            message.data = menu
+            ctx.body = message
+            return ctx
         } else {
             message.code = responseCode.FAIL
             message.message = '未找到id字段'
@@ -111,7 +134,7 @@ exports.delete = async(ctx) => {
  * @returns {Promise.<void>}
  */
 exports.get = async(ctx) => {
-    const data = ctx.body
+    const data = ctx.query
     const message = {}
     try {
         if (data.id) {
@@ -141,13 +164,19 @@ exports.get = async(ctx) => {
  * @returns {Promise.<void>}
  */
 exports.list = async(ctx) => {
-    const data = ctx.body
+    const data = ctx.query
+    if (!data.page) data.page = 1
+    if (!data.size) data.size = 10
     const message = {}
     try {
-        const menu = await Menu.getMenus({})
+        const menu = await Menu.getMenus({}, data.page - 1, data.size)
         if (menu) {
             message.code = responseCode.SUCCESS
             message.message = '获取成功'
+            menu.totalElement = menu.count
+            menu.content = menu.rows
+            delete menu.count
+            delete menu.rows
             message.data = menu
         } else {
             message.code = responseCode.FAIL
