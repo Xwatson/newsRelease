57a666ee053e365deae58bd1d5832c024acb7a90
Xwatson <xuwusheng@hotmail.com>
2017-04-17 23:24:55 +0800

    添加权限路由

--- app/router/index.js
+++ app/router/index.js
@@ -7,8 +7,9 @@ import home from './home'
 import admin from './admin'
 import operation from './operation'
 import menu from './menu'
+import auth from './auth'
 
-const routes = [home, verify, admin, operation, menu]
+const routes = [home, verify, admin, operation, menu, auth]
 export default (app) => {
     routes.forEach((route) => {
         app
