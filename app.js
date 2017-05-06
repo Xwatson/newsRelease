a380f8db71dd0effc370c9ef2f577ac0c1861977
xwatson <wusheng.xu@oborcloud.com>
2017-05-06 22:50:25 +0800

    修改bodyParser

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 import Koa from 'koa'
 const app = new Koa()
+// import path from 'path'
 import views from 'koa-views'
 import convert from 'koa-convert'
 import json from 'koa-json'
 import onerror from 'koa-onerror'
-import Bodyparser from 'koa-bodyparser'
+// import Bodyparser from 'koa-bodyparser'
+import body from 'koa-better-body'
 import logger from 'koa-logger'
 import router from './app/router'
 
 onerror(app)
 
 // middlewares
-app.use(convert(Bodyparser()))
+// app.use(convert(Bodyparser()))
+app.use(convert(body()))
 app.use(convert(json()))
 app.use(convert(logger()))
 app.use(convert(require('koa-static')(__dirname + '/public')))
 
 // 设置Header，这个header会输出给浏览器客户端，表明这个框架是什么生成的，可以自行修改
 app.use(async(ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', 'localhost:3001') // 允许跨域
+    ctx.set('Access-Control-Allow-Origin', 'http://localhost:3001') // 允许跨域
     ctx.set('Access-Control-Allow-Methods', 'GET, POST')
-    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authToken')
+    ctx.set('Access-Control-Allow-Headers', 'Origin, Cache-Control, X-Requested-With, Content-Type, Accept, authToken')
     ctx.set('X-Powered-By', 'Koa2')
+    if (ctx.req.method === 'POST') {
+        ctx.request.body = ctx.request.fields
+    }
     return await next()
 })
 
 app.use(views(__dirname + '/app/views', {
   extension: 'ejs'
 }))
+
+router(app)
 
 // logger
 app.use(async (ctx, next) => {
@@ -37,7 +45,6 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
-router(app)
 
 app.use(async(ctx) => {
     if (ctx.status === 404) {
